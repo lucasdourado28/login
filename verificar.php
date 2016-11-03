b3a67a8116990c37984f4eab42f3158ca160ab5e
lucasdourado28 <lucas28061998@hotmail.com>
2016-11-03 11:02:52 -0200

    Update verificar.php

--- verificar.php
+++ verificar.php
@@ -1,14 +1,10 @@
 <?php
-	$email=$_POST['email'];
-	$senha=$_POST['senha'];
+	$email=$_POST['email']; //peguei o email que o usuario digitou no index.php;
+	$senha=$_POST['senha']; //peguei a senha que o usuario digitou no index.php;
 
 	session_start();
 	if(($email=="emaildoroot")&&($senha=="senharoot")){ /* aqui é o root, entra sem acessar o banco */
-		$_SESSION['login']="adm";
-		$_SESSION['nome']="Administrador";
-		/*echo "Voce logou como ADMINISTRADOR";
-		echo "Variavel recebeu: ".$_SESSION['login'];*/
-
+		$_SESSION['login']="root"; //sessao utilizada para verificar se possui acesso a pagina ou nao;
 
 		header("Location: paginadestino.php");
 	}
@@ -16,40 +12,32 @@
 		//faz a conexao com o banco ao verificar que não é o root
 		include "conexao.php";
 		$sql= "SELECT * FROM tabela WHERE email='$email'"; 
-		$contatos = $fusca -> prepare($sql);
+		$contatos = $conec -> prepare($sql);
 		$contatos -> execute();
 	
-		foreach($contatos as $bolacha){			
-			$email_bd  = $bolacha['email'];
-			$senha_bd  = $bolacha['senha'];
-			$nome_bd  = $bolacha['nome'];
-			$perfil = $bolacha["perfil"];
-			//echo "perfil = ".$perfil;
+		foreach($contatos as $banco){	//passa os dados do banco 		
+			$email_bd  = $banco['email'];//login do banco
+			$senha_bd  = $banco['senha'];//senha do banco
+			$perfil = $banco["perfil"]; //coluna perfil na sua tabela do banco no formato int
+			
 		}
 
 
-		if(($email_bd == $email) && ($senha_bd == $senhaC)){	
-
-			$_SESSION['login']="func";
-			$_SESSION['nome']="funcionário";
+		if(($email_bd == $email) && ($senha_bd == $senhaC)){ //compara o email e senha digitada pelo usuário
 
 			if ($perfil == 0) { /* perfil 0 é o Admin*/
-				$_SESSION['login'] = "adm";
-				$_SESSION['nome'] = $nome_bd;
-				header("Location: ../telaPrincipal.php");
+				$_SESSION['login'] = "usuario0";//sessao utilizada para verificar se possui acesso a pagina ou nao;
+				header("Location: paginadestino.php");
 			}
 
-			else if($perfil == 1){ /* perfil 1 é o funcionário */
-				$_SESSION['login']="funcionario";
-				$_SESSION['nome'] = $nome_bd;
-				// echo "			Voce logou como: FUNCIONÁRIO";
-				// echo "Variavel recebeu: ".$_SESSION['login'];
-				header("Location: ../telaPrincipal.php");
+			else if($perfil == 1){ /* perfil 1 */
+				$_SESSION['login']="usuario1";//sessao utilizada para verificar se possui acesso a pagina ou nao;
+				header("Location: paginadestino.php");
 			}	
 			
 		}
-		else{
-				$_SESSION['login'] = "Login incorreto";
+		else{//se o email ou senha nao existirem no banco
+				$_SESSION['login'] = ""; //sessao utilizada para verificar se possui acesso a pagina ou nao;
 				header("Location: index.php");
 			}	
 	}
