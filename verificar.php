6dd33682c8b0d51498f493e635d5445d888aa377
lucasdourado28 <lucas28061998@hotmail.com>
2016-11-03 10:46:46 -0200

    Update verificar.php

--- verificar.php
+++ verificar.php
@@ -3,51 +3,19 @@
 	$senha=$_POST['senha'];
 
 	session_start();
-	if(($email=="luvithi@autoescola.com")&&($senha=="luvithi")){ /* aqui é o root, o tal do superadmin que entra sem acessar o banco */
+	if(($email=="emaildoroot")&&($senha=="senharoot")){ /* aqui é o root, entra sem acessar o banco */
 		$_SESSION['login']="adm";
 		$_SESSION['nome']="Administrador";
 		/*echo "Voce logou como ADMINISTRADOR";
 		echo "Variavel recebeu: ".$_SESSION['login'];*/
 
-		// seta configurações fusuhorario e tempo limite de inatividade//
-		date_default_timezone_set("Brazil/East");
-		$tempolimite = 3;
-		//fim das configurações de fuso horario e limite de inatividade//
 
-		// aqui ta o seu script de autenticação no momento em que ele for validado você seta as configurações abaixo.//
-		// seta as configurações de tempo permitido para inatividade//
-		 $_SESSION['registro'] = time(); // armazena o momento em que autenticado //
-		 $_SESSION['limite'] = $tempolimite; // armazena o tempo limite sem atividade //
-		// fim das configurações de tempo inativo//
-
-
-
-		header("Location: ../menu.php");
-	}
-	elseif(($email == "func@teste.com") && ($senha=="luvithi")) {
-		$_SESSION['login']="func";
-		$_SESSION['nome']="funcionário";
-
-		// seta configurações fusuhorario e tempo limite de inatividade//
-		date_default_timezone_set("Brazil/East");
-		$tempolimite = 5;
-		//fim das configurações de fuso horario e limite de inatividade//
-
-		// aqui ta o seu script de autenticação no momento em que ele for validado você seta as configurações abaixo.//
-		// seta as configurações de tempo permitido para inatividade//
-		 $_SESSION['registro'] = time(); // armazena o momento em que autenticado //
-		 $_SESSION['limite'] = $tempolimite; // armazena o tempo limite sem atividade //
-		// fim das configurações de tempo inativo//
-
-
-		header("Location: ../menu.php");
+		header("Location: paginadestino.php");
 	}
 	else{
-
-		$senhaC = md5($senha);
-
+		//faz a conexao com o banco ao verificar que não é o root
 		include "conexao.php";
-		$sql= "SELECT * FROM usuarios_tb WHERE email='$email'";
+		$sql= "SELECT * FROM tabela WHERE email='$email'"; 
 		$contatos = $fusca -> prepare($sql);
 		$contatos -> execute();
 	
@@ -64,19 +32,6 @@
 
 			$_SESSION['login']="func";
 			$_SESSION['nome']="funcionário";
-
-			// seta configurações fusuhorario e tempo limite de inatividade//
-			date_default_timezone_set("Brazil/East");
-			$tempolimite = 5;
-			//fim das configurações de fuso horario e limite de inatividade//
-
-			// aqui ta o seu script de autenticação no momento em que ele for validado você seta as configurações abaixo.//
-			// seta as configurações de tempo permitido para inatividade//
-			 $_SESSION['registro'] = time(); // armazena o momento em que autenticado //
-			 $_SESSION['limite'] = $tempolimite; // armazena o tempo limite sem atividade //
-			// fim das configurações de tempo inativo//	
-			
-			
 
 			if ($perfil == 0) { /* perfil 0 é o Admin*/
 				$_SESSION['login'] = "adm";
